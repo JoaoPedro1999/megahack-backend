0a3c3a7e4e3c1fac3b824223ee393a7c08f1bde5
João Pedro <joaopedrobeckland@gmail.com>
2020-04-05 11:36:13 -0300

    Initial config

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'gohealth',
+  database: 'gohealth',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
